0bb4b1638eb56b6d5aa703f176e8185cba43cdeb
Baesojeong2015 <107821802+Baesojeong2015@users.noreply.github.com>
2022-06-30 18:46:33 +0900

    Create TaxiTry.java

---
+++ bushomework/TaxiTry.java
@@ -0,0 +1,10 @@
+public class TaxiTry {
+    public static void main(String[] args) {
+        Taxi taxi = new Taxi();
+        taxi.driving(13);
+        taxi.getInTaxi();
+        taxi.newSpeed(30);
+        taxi.addPrice(7);
+
+    }
+}
